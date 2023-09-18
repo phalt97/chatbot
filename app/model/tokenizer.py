49e62641327f091797bbfd8bc763ac5119cb4f2e
phalt97 <phalt22197@gmail.com>
2023-09-18 14:18:54 +0700

    fix bug

--- app/model/tokenizer.py
+++ app/model/tokenizer.py
@@ -68,6 +68,7 @@ def unicodeToAscii(s):
 def normalizeString(s):
     s = unicodeToAscii(s.lower().strip())
     s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", s)
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     s = re.sub(r"\s+", r" ", s).strip()
     return s
